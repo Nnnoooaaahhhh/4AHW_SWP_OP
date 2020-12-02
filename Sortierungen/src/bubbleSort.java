6ca1267a6cd791231349a5e49c701fcf5fd3caef
Nnnoooaaahhhh <55807513+Nnnoooaaahhhh@users.noreply.github.com>
2020-12-02 16:41:24 +0100

    Add files via upload

--- Sortierungen/src/bubbleSort.java
+++ Sortierungen/src/bubbleSort.java
@@ -5,28 +5,43 @@ public class bubbleSort {
 	void bubSort(int number){
 		long start;
 		long end;
-		int numbers[] = new int[number];
-		Random rnd = new Random();
-		for(int i = 0; i < numbers.length; i++) {
-			numbers[i] = rnd.nextInt(number);
-		}
-		
+		int index;
 		int temp;
 		int steps = 0;
+		int orderedNumbers[] = new int[number];
+		int endArr[] = new int[number];
+		Random rnd = new Random();
+		
+		
+		for(int i = 0; i < orderedNumbers.length; i++) {
+			orderedNumbers[i] = i;
+		}
+		
+		for(int i = 0; i < endArr.length; i++) {
+			index = rnd.nextInt(orderedNumbers.length-i);
+			endArr[i] = orderedNumbers[index];
+			temp = orderedNumbers[orderedNumbers.length-i-1];
+			orderedNumbers[orderedNumbers.length-i-1] = orderedNumbers[index];
+			orderedNumbers[index] = temp;
+			
+			
+			
+		}
+		
 		start = System.nanoTime();
-		for(int i = 0; i < numbers.length; i++) {
-			for(int j = 0; j < numbers.length-i-1; j++) {
-				if(numbers[j+1] < numbers[j]) {
-					temp = numbers[j];
-					numbers[j] = numbers[j+1];
-					numbers[j+1] = temp;
+		for(int i = 0; i < endArr.length; i++) {
+			for(int j = 0; j < endArr.length-i-1; j++) {
+				if(endArr[j+1] < endArr[j]) {
+					temp = endArr[j];
+					endArr[j] = endArr[j+1];
+					endArr[j+1] = temp;
 					steps++;
 				}
 			}
 		}
 		end = System.nanoTime();
-		for(int i = 0; i < numbers.length; i++) {
-			System.out.println(numbers[i]);
+		for(int i = 0; i < endArr.length; i++) {
+			System.out.println(endArr[i]);
 			}
 		System.out.println("Time: " + (end-start) + " Nanoseconds in " + steps + " steps");
 	}
--- Sortierungen/src/testMain.java
+++ Sortierungen/src/testMain.java
@@ -3,6 +3,6 @@ public class testMain {
 
 	public static void main(String[] args) {
 		bubbleSort a = new bubbleSort();
-		a.bubSort(100);
+		a.bubSort(1000);
 	}
 }
